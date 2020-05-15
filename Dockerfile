085cee95432194819e5a02bdc4d73b247872d380
Miro Spönemann <miro.spoenemann@typefox.io>
2020-05-15 08:53:14 +0000

    Updated server image, added link for publishing info

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Snapshot revision: df295b2
+# Snapshot revision: 951d773
 FROM docker.pkg.github.com/eclipse/openvsx/openvsx-server:snapshot
 
 ADD website/static/* BOOT-INF/classes/static/
--- website/src/about.tsx
+++ website/src/about.tsx
@@ -9,12 +9,15 @@
  ********************************************************************************/
 
 import * as React from 'react';
-import { Link, Box, Typography, Container, useTheme, makeStyles } from '@material-ui/core';
+import { Link, Typography, Container, useTheme, makeStyles } from '@material-ui/core';
 
 const About = () => {
     const theme = useTheme();
     const body1 = theme.typography.body1;
     const classes = makeStyles({
+        heading: {
+            marginTop: theme.spacing(4)
+        },
         paragraph: {
             marginTop: theme.spacing(2)
         },
@@ -26,37 +29,41 @@ const About = () => {
         }
     })();
     return <Container maxWidth='md'>
-        <Box mt={4}>
-            <Typography variant='h4'>About This Service</Typography>
-            <Typography variant='body1' className={classes.paragraph}>
-                Open VSX is an open-source registry for VS Code extensions.
-                It can be used by any development environment that supports such extensions.
-                See <Link color='secondary' href='https://www.eclipse.org/community/eclipse_newsletter/2020/march/1.php'>this article</Link> for
-                more information.
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph}>
-                This website is currently operated by <Link color='secondary' href='https://www.typefox.io/'>TypeFox</Link>,
-                but it will soon be transferred to the <Link color='secondary' href='https://www.eclipse.org/'>Eclipse Foundation</Link>.
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph}>
-                The source code of this website is managed by
-                the <Link color='secondary' href='https://projects.eclipse.org/projects/ecd.openvsx'>Eclipse Open VSX</Link> project
-                and is licensed under
-                the <Link color='secondary' href='https://www.eclipse.org/legal/epl-2.0/'>Eclipse Public License v2.0</Link>.
-                The code split in two repositories:
-            </Typography>
-            <ul className={classes.adaptFont}>
-                <li>
-                    <Link color='secondary' href='https://github.com/eclipse/openvsx'>eclipse/openvsx</Link> &ndash;
-                    main code with server, web UI and CLI. These components can be reused to deploy other registry instances
-                    (both public and private).
-                </li>
-                <li>
-                    <Link color='secondary' href='https://github.com/eclipse/open-vsx.org'>eclipse/open-vsx.org</Link> &ndash;
-                    additional code for this website.
-                </li>
-            </ul>
-        </Box>
+        <Typography variant='h4' className={classes.heading}>About This Service</Typography>
+        <Typography variant='body1' className={classes.paragraph}>
+            Open VSX is an open-source registry for VS Code extensions.
+            It can be used by any development environment that supports such extensions.
+            See <Link color='secondary' href='https://www.eclipse.org/community/eclipse_newsletter/2020/march/1.php'>this article</Link> for
+            more information.
+        </Typography>
+        <Typography variant='body1' className={classes.paragraph}>
+            This website is currently operated by <Link color='secondary' href='https://www.typefox.io/'>TypeFox</Link>,
+            but it will soon be transferred to the <Link color='secondary' href='https://www.eclipse.org/'>Eclipse Foundation</Link>.
+        </Typography>
+        <Typography variant='body1' className={classes.paragraph}>
+            The source code of this website is managed by
+            the <Link color='secondary' href='https://projects.eclipse.org/projects/ecd.openvsx'>Eclipse Open VSX</Link> project
+            and is licensed under
+            the <Link color='secondary' href='https://www.eclipse.org/legal/epl-2.0/'>Eclipse Public License v2.0</Link>.
+            The code split in two repositories:
+        </Typography>
+        <ul className={classes.adaptFont}>
+            <li>
+                <Link color='secondary' href='https://github.com/eclipse/openvsx'>eclipse/openvsx</Link> &ndash;
+                main code with server, web UI and CLI. These components can be reused to deploy other registry instances
+                (both public and private).
+            </li>
+            <li>
+                <Link color='secondary' href='https://github.com/eclipse/open-vsx.org'>eclipse/open-vsx.org</Link> &ndash;
+                additional code for this website.
+            </li>
+        </ul>
+
+        <Typography variant='h5' className={classes.heading}>Publishing Extensions</Typography>
+        <Typography variant='body1' className={classes.paragraph}>
+            The publishing process is described in
+            the <Link color='secondary' href='https://github.com/eclipse/openvsx/wiki/Publishing-Extensions#how-to-publish-an-extension'>openvsx Wiki</Link>.
+        </Typography>
     </Container>;
 }
 
