927c15ee4a3b9f069176e2f6507f1733ec6f9684
amvanbaren <aart.vanbaren@eclipse-foundation.org>
2022-12-02 18:19:52 +0200

    Remove leading space from menu-content.tsx

--- website/src/menu-content.tsx
+++ website/src/menu-content.tsx
@@ -8,159 +8,158 @@
  * SPDX-License-Identifier: EPL-2.0
  ********************************************************************************/
 
- import * as React from 'react';
- import { makeStyles } from '@material-ui/styles';
- import { Theme, Typography, MenuItem, Link, Button } from '@material-ui/core';
- import { Link as RouteLink } from 'react-router-dom';
- import GitHubIcon from '@material-ui/icons/GitHub';
- import MenuBookIcon from '@material-ui/icons/MenuBook';
- import HelpIcon from '@material-ui/icons/Help';
- import ForumIcon from '@material-ui/icons/Forum';
- import InfoIcon from '@material-ui/icons/Info';
- import StarIcon from '@material-ui/icons/Star';
- import StatusIcon from '@material-ui/icons/NetworkCheck';
- import PublishIcon from '@material-ui/icons/Publish';
- import { UserSettingsRoutes } from 'openvsx-webui';
- 
- const menuContentStyle = makeStyles((theme: Theme) => ({
-     headerItem: {
-         margin: theme.spacing(2.5),
-         color: theme.palette.text.primary,
-         textDecoration: 'none',
-         fontSize: '1.1rem',
-         fontFamily: theme.typography.fontFamily,
-         fontWeight: theme.typography.fontWeightLight,
-         letterSpacing: 1,
-         '&:hover': {
-             color: theme.palette.secondary.main,
-             textDecoration: 'none'
-         }
-     },
-     publishButton: {
-         marginLeft: theme.spacing(2.5),
-         marginRight: theme.spacing(2.5)
-     },
-     menuItem: {
-         cursor: 'auto',
-         '&>a': {
-             textDecoration: 'none'
-         }
-     },
-     itemIcon: {
-         marginRight: theme.spacing(1),
-         width: '16px',
-         height: '16px',
-     },
-     alignVertically: {
-         display: 'flex',
-         alignItems: 'center'
-     }
- }));
- 
- 
- //-------------------- Mobile View --------------------//
- 
- export const MobileMenuContent: React.FunctionComponent = () => {
-     const classes = menuContentStyle();
-     return <React.Fragment>
-         <MenuItem className={classes.menuItem}>
-             <Link target='_blank' href='https://github.com/eclipse/openvsx'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <GitHubIcon className={classes.itemIcon} />
-                     Source Code
-                 </Typography>
-             </Link>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <Link href='https://github.com/eclipse/openvsx/wiki'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <MenuBookIcon className={classes.itemIcon} />
-                     Documentation
-                 </Typography>
-             </Link>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <Link href='https://status.open-vsx.org/'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <StatusIcon className={classes.itemIcon} />
-                     Status
-                 </Typography>
-             </Link>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <Link href='https://www.eclipse.org/legal/open-vsx-registry-faq/'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <HelpIcon className={classes.itemIcon} />
-                     FAQ
-                 </Typography>
-             </Link>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <Link href='https://gitter.im/eclipse/openvsx'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <ForumIcon className={classes.itemIcon} />
-                     Community
-                 </Typography>
-             </Link>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <RouteLink to='/about'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <InfoIcon className={classes.itemIcon} />
-                     About
-                 </Typography>
-             </RouteLink>
-         </MenuItem>
-         <MenuItem className={classes.menuItem}>
-             <Link href='https://www.eclipse.org/donate/openvsx/'>
-                 <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                     <StarIcon className={classes.itemIcon} />
-                     Sponsor
-                 </Typography>
-             </Link>
-         </MenuItem>
-         {
-             !location.pathname.startsWith(UserSettingsRoutes.ROOT)
-             ? <MenuItem className={classes.menuItem}>
-                 <RouteLink to='/user-settings/extensions'>
-                     <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
-                         <PublishIcon className={classes.itemIcon} />
-                         Publish Extension
-                     </Typography>
-                 </RouteLink>
-             </MenuItem>
-             : null
-         }
-     </React.Fragment>;
- }
- 
- 
- //-------------------- Default View --------------------//
- 
- export const DefaultMenuContent: React.FunctionComponent = () => {
-     const classes = menuContentStyle();
-     return <React.Fragment>
-         <Link href='https://github.com/eclipse/openvsx/wiki' className={classes.headerItem}>
-             Documentation
-         </Link>
-         <Link href='https://www.eclipse.org/legal/open-vsx-registry-faq/' className={classes.headerItem}>
-             FAQ
-         </Link>
-         <Link href='https://status.open-vsx.org/' className={classes.headerItem}>
-             Status
-         </Link>
-         <Link href='https://gitter.im/eclipse/openvsx' className={classes.headerItem}>
-             Community
-         </Link>
-         <RouteLink to='/about' className={classes.headerItem}>
-             About
-         </RouteLink>
-         <Link href='https://www.eclipse.org/donate/openvsx/' className={classes.headerItem}>
-             Sponsor
-         </Link>
-         <Button variant='contained' color='secondary' href='/user-settings/extensions' className={classes.publishButton}>
-             Publish
-         </Button>
-     </React.Fragment>;
- }
- 
+import * as React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Theme, Typography, MenuItem, Link, Button } from '@material-ui/core';
+import { Link as RouteLink } from 'react-router-dom';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import HelpIcon from '@material-ui/icons/Help';
+import ForumIcon from '@material-ui/icons/Forum';
+import InfoIcon from '@material-ui/icons/Info';
+import StarIcon from '@material-ui/icons/Star';
+import StatusIcon from '@material-ui/icons/NetworkCheck';
+import PublishIcon from '@material-ui/icons/Publish';
+import { UserSettingsRoutes } from 'openvsx-webui';
+
+const menuContentStyle = makeStyles((theme: Theme) => ({
+    headerItem: {
+        margin: theme.spacing(2.5),
+        color: theme.palette.text.primary,
+        textDecoration: 'none',
+        fontSize: '1.1rem',
+        fontFamily: theme.typography.fontFamily,
+        fontWeight: theme.typography.fontWeightLight,
+        letterSpacing: 1,
+        '&:hover': {
+            color: theme.palette.secondary.main,
+            textDecoration: 'none'
+        }
+    },
+    publishButton: {
+        marginLeft: theme.spacing(2.5),
+        marginRight: theme.spacing(2.5)
+    },
+    menuItem: {
+        cursor: 'auto',
+        '&>a': {
+            textDecoration: 'none'
+        }
+    },
+    itemIcon: {
+        marginRight: theme.spacing(1),
+        width: '16px',
+        height: '16px',
+    },
+    alignVertically: {
+        display: 'flex',
+        alignItems: 'center'
+    }
+}));
+
+
+//-------------------- Mobile View --------------------//
+
+export const MobileMenuContent: React.FunctionComponent = () => {
+    const classes = menuContentStyle();
+    return <React.Fragment>
+        <MenuItem className={classes.menuItem}>
+            <Link target='_blank' href='https://github.com/eclipse/openvsx'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <GitHubIcon className={classes.itemIcon} />
+                    Source Code
+                </Typography>
+            </Link>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <Link href='https://github.com/eclipse/openvsx/wiki'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <MenuBookIcon className={classes.itemIcon} />
+                    Documentation
+                </Typography>
+            </Link>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <Link href='https://status.open-vsx.org/'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <StatusIcon className={classes.itemIcon} />
+                    Status
+                </Typography>
+            </Link>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <Link href='https://www.eclipse.org/legal/open-vsx-registry-faq/'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <HelpIcon className={classes.itemIcon} />
+                    FAQ
+                </Typography>
+            </Link>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <Link href='https://gitter.im/eclipse/openvsx'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <ForumIcon className={classes.itemIcon} />
+                    Community
+                </Typography>
+            </Link>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <RouteLink to='/about'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <InfoIcon className={classes.itemIcon} />
+                    About
+                </Typography>
+            </RouteLink>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+            <Link href='https://www.eclipse.org/donate/openvsx/'>
+                <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                    <StarIcon className={classes.itemIcon} />
+                    Sponsor
+                </Typography>
+            </Link>
+        </MenuItem>
+        {
+            !location.pathname.startsWith(UserSettingsRoutes.ROOT)
+            ? <MenuItem className={classes.menuItem}>
+                <RouteLink to='/user-settings/extensions'>
+                    <Typography variant='body2' color='textPrimary' className={classes.alignVertically}>
+                        <PublishIcon className={classes.itemIcon} />
+                        Publish Extension
+                    </Typography>
+                </RouteLink>
+            </MenuItem>
+            : null
+        }
+    </React.Fragment>;
+}
+
+
+//-------------------- Default View --------------------//
+
+export const DefaultMenuContent: React.FunctionComponent = () => {
+    const classes = menuContentStyle();
+    return <React.Fragment>
+        <Link href='https://github.com/eclipse/openvsx/wiki' className={classes.headerItem}>
+            Documentation
+        </Link>
+        <Link href='https://www.eclipse.org/legal/open-vsx-registry-faq/' className={classes.headerItem}>
+            FAQ
+        </Link>
+        <Link href='https://status.open-vsx.org/' className={classes.headerItem}>
+            Status
+        </Link>
+        <Link href='https://gitter.im/eclipse/openvsx' className={classes.headerItem}>
+            Community
+        </Link>
+        <RouteLink to='/about' className={classes.headerItem}>
+            About
+        </RouteLink>
+        <Link href='https://www.eclipse.org/donate/openvsx/' className={classes.headerItem}>
+            Sponsor
+        </Link>
+        <Button variant='contained' color='secondary' href='/user-settings/extensions' className={classes.publishButton}>
+            Publish
+        </Button>
+    </React.Fragment>;
+}
