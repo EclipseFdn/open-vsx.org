a30bc0bfa665459fe4082a18abcbd950f639ed0f
amvanbaren <aart.vanbaren@eclipse-foundation.org>
2025-02-13 11:39:20 +0200

    Hardcode canLogin

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -42,6 +42,7 @@ const App: FunctionComponent = () => {
                 <Main
                     service={service}
                     pageSettings={pageSettings}
+                    canLogin
                 />
             </ThemeProvider>
         </HelmetProvider>
