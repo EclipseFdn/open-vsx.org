175fead295c1ce7df35a0724a8f2d8f3017713e0
escbeargew99-stack <escbeargew99@gmail.com>
2025-09-25 06:24:02 +0700

    Update index.js

--- website/dev/server/index.js
+++ website/dev/server/index.js
@@ -1,4 +1,4 @@
-/********************************************************************************
+/******************************************************************************** 
  * Copyright (c) 2020 TypeFox and others
  * 
  * This program and the accompanying materials are made available under the
