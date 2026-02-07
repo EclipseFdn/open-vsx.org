34b9de48a44b10284ed39bbd73b0e59480ae7590
Manpreet Kaur <manpreet.kaur@eclipse-foundation.org>
2026-02-07 05:06:26 -0500

    make dependencies conditional and added charts for alb

--- kubernetes/helm-deploy.sh
+++ kubernetes/helm-deploy.sh
@@ -66,5 +66,8 @@ mkdir -p "${HELM_DATA_HOME}"
 
 helm version
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add postgresql https://charts.bitnami.com/bitnami
+helm repo add eks https://aws.github.io/eks-charts
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
 helm dependency build  "${ROOT_DIR}/charts/openvsx"
 helm upgrade --install "${release_name}" "${ROOT_DIR}/charts/openvsx" -f "${values_file}" --set image.tag="${image_tag}" --namespace "${namespace}"
