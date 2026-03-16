0488b48edb064477a67efc7b3869922dafd76df8
Manpreet Kaur <manpreet.kaur@eclipse-foundation.org>
2026-03-16 14:37:45 -0400

    Added external dns back to support the move to fastly

--- kubernetes/helm-deploy.sh
+++ kubernetes/helm-deploy.sh
@@ -74,5 +74,6 @@ helm version
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add postgresql-ha https://charts.bitnami.com/bitnami
 helm repo add eks https://aws.github.io/eks-charts
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
 helm dependency build  "${ROOT_DIR}/charts/openvsx"
 helm upgrade --install "${release_name}" "${ROOT_DIR}/charts/openvsx" -f "${values_file}" --set image.tag="${image_tag}" --namespace "${namespace}" --create-namespace --force-conflicts
